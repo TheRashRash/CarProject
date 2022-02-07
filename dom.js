c8098d48d3f92f7045a73f9f2c44e31a6db8c553
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-07 14:25:38 +0000

    adding basic layout and get/create garage functionality

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+//Read Garage(READ) field
+export const readGarages = document.getElementById(`readGarages`);
+//Create Garage(POST) field
+export const garageName = document.getElementById(`garageName`);
+export const createGarageButton = document.getElementById(`createGarageButton`);
+export const garageOutput = document.getElementById(`garageOutput`);
+//Create Car (POST) field
+export const carName = document.getElementById(`carName`);
+export const garageID = document.getElementById(`garageID`);
+export const createcarButton = document.getElementById(`createCarButton`);
