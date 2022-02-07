d3dd14d5a7151a3531a3a1ebce4f5afb9ed4407f
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-07 17:06:58 +0000

    create method complete

--- dom.js
+++ dom.js
@@ -1,6 +1,8 @@
 
 //Read Garage(READ) field
 export const readGarages = document.getElementById(`readGarages`);
+//Read Cars(READ) field
+export const readCars = document.getElementById(`readCars`);
 //Create Garage(POST) field
 export const garageName = document.getElementById(`garageName`);
 export const createGarageButton = document.getElementById(`createGarageButton`);
@@ -8,4 +10,6 @@ export const garageOutput = document.getElementById(`garageOutput`);
 //Create Car (POST) field
 export const carName = document.getElementById(`carName`);
 export const garageID = document.getElementById(`garageID`);
-export const createcarButton = document.getElementById(`createCarButton`);
+export const numDoors = document.getElementById(`numDoors`);
+export const createCarButton = document.getElementById(`createCarButton`);
+export const carOutput = document.getElementById(`carOutput`);
--- script.js
+++ script.js
@@ -4,7 +4,8 @@ import * as DOM from './dom.js';
 
 DOM.createGarageButton.onclick = () => createGarage();
 DOM.readGarages.onclick = () => getGarage();
-// DOM.createCarButton.onclick = () => carCreate();
+DOM.readCars.onclick = () => carRead();
+DOM.createCarButton.onclick = () => carCreate();
 
 const getGarage = () => {
   DOM.garageOutput.innerHTML = ``;
@@ -29,12 +30,36 @@ const createGarage = () => {
     });
 }
 
-// const carCreate = () => {
-//   axios.post(`http://localhost:8080/car/create`, {name : DOM.carName.value, garage : DOM.garageID.value})
-//     .then((response) => {
-//       console.log(response);
-//       getGarage();
-//     }).catch((err) => {
-//       console.log(err);
-//     });
-// }
+const carRead = () => {
+  DOM.carOutput.innerHTML = ``;
+
+  axios.get(`http://localhost:8080/car/read`)
+    .then((response) => {
+      console.log(response);
+      DOM.carOutput.innerHTML = `${JSON.stringify(response.data)}`;
+    }).catch((err) => {
+      console.log(err);
+    });
+}
+
+const carCreate = () => {
+  axios.post(`http://localhost:8080/car/create`, {
+    name : "ferrari",
+    colour : "red",
+    make : "aeba",
+    model : "srs",
+    doors : 3,
+    garage: {id : 7, name: "new"}
+
+  })
+    .then((response) => {
+      console.log(response);
+      carRead();
+    }).catch((err) => {
+      console.log(err.response.data);
+    });
+}
+// axios.post(`http://localhost:8080/car/create`, {name : DOM.carName.value, garage: {id : DOM.garageID.value}})
+//garage: {id : DOM.garageID.value}
+
+
